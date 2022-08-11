fb0802e7627d1c36fe4ca14e0305d65994d19899
Aman verma <88731279+AmanVgit@users.noreply.github.com>
2022-08-11 23:23:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var x= Math.random();
+var y= Math.floor((x*6)+1);
+var randomImage = "dice"+ y + ".png";
+var randomImageSource = "images" + '\\'+ randomImage
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+ 
+var a = Math.random();
+var b = Math.floor((a*6)+1);
+var randomImage2 = "dice" + b + ".png";
+var randomImageSource2 = "images" + '\\' + randomImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+if(b<y)
+{
+document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+}
+else if(b>y)
+{
+    document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
+}
+else
+{
+    document.querySelector("h1").innerHTML = "🚩DRAW🚩";
+}
