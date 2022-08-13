f23f9364fd937c6c22c61545ccffbbd8196b9dba
Aman verma <88731279+AmanVgit@users.noreply.github.com>
2022-08-13 17:14:31 +0530

    Added js for button

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var x= Math.random();
-var y= Math.floor((x*6)+1);
-var randomImage = "dice"+ y + ".png";
-var randomImageSource = "images" + '\\'+ randomImage
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
- 
-var a = Math.random();
-var b = Math.floor((a*6)+1);
-var randomImage2 = "dice" + b + ".png";
-var randomImageSource2 = "images" + '\\' + randomImage2;
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImageSource2);
+var btn = document.getElementById('btn');
+btn.addEventListener("click", function refresh()
+{
 
-if(b<y)
-{
-document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+    var x = Math.random();
+    var y = Math.floor((x * 6) + 1);
+    var randomImage = "dice" + y + ".png";
+    var randomImageSource = "images" + '\\' + randomImage
+    var image1 = document.querySelectorAll("img")[0];
+    image1.setAttribute("src", randomImageSource);
+
+    var a = Math.random();
+    var b = Math.floor((a * 6) + 1);
+    var randomImage2 = "dice" + b + ".png";
+    var randomImageSource2 = "images" + '\\' + randomImage2;
+    var image2 = document.querySelectorAll("img")[1];
+    image2.setAttribute("src", randomImageSource2);
+});
+
+if (b < y) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
 }
-else if(b>y)
-{
+else if (b > y) {
     document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
 }
-else
-{
+else {
     document.querySelector("h1").innerHTML = "🚩DRAW🚩";
 }
